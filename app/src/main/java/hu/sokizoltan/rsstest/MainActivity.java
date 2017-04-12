62052ab7219f103623f31ae092ccd906aa889922
Suppoze <tegyukfel@gmail.com>
2017-04-12 14:43:19 +0200

    Main activity draft

--- app/src/main/java/hu/sokizoltan/rsstest/MainActivity.java
+++ app/src/main/java/hu/sokizoltan/rsstest/MainActivity.java
@@ -1,13 +1,43 @@
 package hu.sokizoltan.rsstest;
 
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
+import butterknife.BindView;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.main_activity_appbar)
+    AppBarLayout appBarLayout;
+
+    @BindView(R.id.main_activity_tablayout)
+    TabLayout tabLayout;
+
+    @BindView(R.id.activity_main_viewpager)
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeActionBar();
+        initializeTabLayout();
+        initializeViewPager();
+    }
+
+    private void initializeActionBar() {
+
+    }
+
+    private void initializeTabLayout() {
+
+    }
+
+    private void initializeViewPager() {
+
     }
 }
